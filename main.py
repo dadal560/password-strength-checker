15eccff111d086d186cbebc011da714a232ba99f
dadal <gwen.henry56@gmail.com>
2025-07-21 13:43:03 +0200

    BOUCLE POUR avoir un mdp

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def mdp():
-    print("This is the MDP function.")
     mdp = input("Please enter the MDP value: ")
+    while not mdp:
+        print("No MDP value provided. Please try again.")
+        mdp = input("Please enter the MDP value: ")
     return mdp
 
 
 if __name__ == "__main__":
     result = mdp()
-    print(result)
+    print(f"Mot de passe saisi : {result}")
