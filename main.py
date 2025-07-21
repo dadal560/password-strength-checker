0eb46cb4069f6c1d58409c26f4de69ea56997de5
dadal <gwen.henry56@gmail.com>
2025-07-21 13:37:42 +0200

    test mdp

---
+++ main.py
@@ -0,0 +1,9 @@
+def mdp():
+    print("This is the MDP function.")
+    mdp = input("Please enter the MDP value: ")
+    return mdp
+
+
+if __name__ == "__main__":
+    result = mdp()
+    print(result)
